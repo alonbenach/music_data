ce0168e5284b4eeb5d73d563d8815c60eb54528c
alonbenach <benach@gmail.com>
2023-11-12 23:23:44 +0100

    testing

--- scraper_top100.py
+++ scraper_top100.py
@@ -29,3 +29,134 @@ for date in daterange:
 for key in charts:
     charts[key] = pd.DataFrame.from_dict(charts[key])
 # %%
+
+# from streamlit_jupyter
+# import StreamlitPatcher, tqdm StreamlitPatcher().jupyter()
+# -
+
+st.write("Here's our first attempt at using data to create a table:")
+st.write(
+    pd.DataFrame({"first column": [1, 2, 3, 4], "second column": [10, 20, 30, 40]})
+)
+
+st.markdown("*Streamlit* is **really** ***cool***.")
+st.markdown(
+    """
+    :red[Streamlit] :orange[can] :green[write] :blue[text] :violet[in]
+    :gray[pretty] :rainbow[colors]."""
+)
+st.markdown(
+    "Here's a bouquet &mdash;\
+            :tulip::cherry_blossom::rose::hibiscus::sunflower::blossom:"
+)
+
+multi = """If you end a line with two spaces,
+a soft return is used for the next line.
+
+Two (or more) newline characters in a row will result in a hard return.
+"""
+st.markdown(multi)
+
+
+# +
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+
+st.line_chart(chart_data)
+
+# +
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4], columns=["lat", "lon"]
+)
+
+st.map(map_data)
+# -
+
+
+x = st.slider("x")  # 👈 this is a widget
+st.write(x, "squared is", x * x)
+
+
+if st.checkbox("Show dataframe"):
+    chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+
+    chart_data
+
+
+df = pd.DataFrame({"first column": [1, 2, 3, 4], "second column": [10, 20, 30, 40]})
+
+option = st.selectbox("Which number do you like best?", df["first column"])
+
+"You selected: ", option
+
+
+# Add a selectbox to the sidebar:
+add_selectbox = st.sidebar.selectbox(
+    "How would you like to be contacted?", ("Email", "Home phone", "Mobile phone")
+)
+
+# Add a slider to the sidebar:
+add_slider = st.sidebar.slider("Select a range of values", 0.0, 100.0, (25.0, 75.0))
+
+
+st.markdown("# Main page 🎈")
+st.sidebar.markdown("# Main page 🎈")
+
+
+st.markdown("# Page 2 ❄️")
+st.sidebar.markdown("# Page 2 ❄️")
+
+
+# Add histogram data
+x1 = np.random.randn(200) - 2
+x2 = np.random.randn(200)
+x3 = np.random.randn(200) + 2
+
+# Group data together
+hist_data = [x1, x2, x3]
+
+group_labels = ["Group 1", "Group 2", "Group 3"]
+
+# Create distplot with custom bin_size
+fig = ff.create_distplot(hist_data, group_labels, bin_size=[0.1, 0.25, 0.5])
+
+# Plot!
+st.plotly_chart(fig, use_container_width=True)
+
+df = px.data.gapminder()
+
+fig = px.scatter(
+    df.query("year==2007"),
+    x="gdpPercap",
+    y="lifeExp",
+    size="pop",
+    color="continent",
+    hover_name="country",
+    log_x=True,
+    size_max=60,
+)
+
+tab1, tab2 = st.tabs(["Streamlit theme (default)", "Plotly native theme"])
+with tab1:
+    # Use the Streamlit theme.
+    # This is the default. So you can also omit the theme argument.
+    st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+with tab2:
+    # Use the native Plotly theme.
+    st.plotly_chart(fig, theme=None, use_container_width=True)
+
+
+st.subheader("Define a custom colorscale")
+df = px.data.iris()
+fig = px.scatter(
+    df,
+    x="sepal_width",
+    y="sepal_length",
+    color="sepal_length",
+    color_continuous_scale="reds",
+)
+
+tab1, tab2 = st.tabs(["Streamlit theme (default)", "Plotly native theme"])
+with tab1:
+    st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+with tab2:
+    st.plotly_chart(fig, theme=None, use_container_width=True)
