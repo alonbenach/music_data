03bfe5dbe48208d09832dfbdba7282a2b38a1f26
alonbenach <benach@gmail.com>
2023-12-17 00:10:19 +0100

    add genre

--- scraper_top100.py
+++ scraper_top100.py
@@ -2,7 +2,10 @@
 import billboard
 from datetime import date
 import pandas as pd
+import requests
+from bs4 import BeautifulSoup
 
+# %%
 start_year = 1990
 end_year = 2022
 
@@ -71,5 +74,61 @@ print(songs_df)
 # %%
 songs_df.to_excel("songs_df.xlsx", index=False)
 
+# %%
+# Load the data
+excel_file_name = "songs_df.xlsx"
+# Read the Excel file into a dictionary of dataframes
+songs_df = pd.read_excel(excel_file_name)
 
+
+# %%
+# Function to generate Wikipedia URLs for artists
+def generate_wikipedia_urls(artist):
+    # Replace spaces with underscores and create Wikipedia URL
+    return f"https://en.wikipedia.org/wiki/{artist.replace(' ', '_')}"
+
+
+# Add a new column 'Wikipedia_Page' with Wikipedia URLs
+songs_df["Wikipedia_Page"] = songs_df["Artist"].apply(generate_wikipedia_urls)
+
+# Display the updated DataFrame
+print(songs_df)
+
+
+# %%
+# Function to scrape artist's genre from Wikipedia
+def scrape_artist_genre(url):
+    try:
+        response = requests.get(url, timeout=10)  # Set a timeout to prevent hanging
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.content, "html.parser")
+            infobox = soup.find("table", {"class": "infobox"})
+            if infobox:
+                rows = infobox.find_all("tr")
+                for row in rows:
+                    if row.th and row.th.text.strip() == "Genres":
+                        genres = row.td.text.strip()
+                        return genres
+        return None
+    except requests.RequestException as e:
+        print(f"Request Exception: {e}")
+        return None
+
+
+# Iterate through each row and fetch genre for each artist's Wikipedia page
+for index, row in songs_df.iterrows():
+    url = row["Wikipedia_Page"]
+    artist = row["Artist"]
+    genre = scrape_artist_genre(url)
+    songs_df.loc[songs_df["Artist"] == artist, "Genre"] = genre
+    print(f"Artist: {artist} - Genre: {genre}")
+
+# Display the updated DataFrame
+print(songs_df["Genre"])
+# %%
+genre_df = songs_df[["Artist", "Genre"]]
+genre_df = genre_df.drop_duplicates(subset=["Artist"], keep="first")
+
+# %%
+print(genre_df["Artist"].count() - genre_df["Genre"].count(), "songs have null genre.")
 # %%
